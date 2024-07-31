6385f60545a9acda8b9e311357c9e7288814e23f
230500226 <125464717+230500226@users.noreply.github.com>
2024-07-31 10:16:52 +0200

    netbeans init

---
+++ src/main/java/com/mycompany/javaex3/JavaEx3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaex3;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaEx3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
